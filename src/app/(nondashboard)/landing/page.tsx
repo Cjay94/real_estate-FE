af3a9980b9de237f73a487d77f210cad5cd04541
Cjay94 <cjephthah94@gmail.com>
2025-02-26 12:29:40 +0200

    Landing page pt2: Basic HeroSection Component created

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from './HeroSection'
 
 const Landing = () => {
     return (
         <section>
-            Landing
+            <HeroSection />
         </section>
     )
 }
